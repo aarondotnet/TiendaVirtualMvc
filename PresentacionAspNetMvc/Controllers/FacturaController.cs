f085a220cf538b0d15ce4cee6ad96aa3d870fc92
Aaron <2d9ph7@svk.jp>
2018-02-15 14:58:32 +0100

    Agregar archivos de proyecto.

---
+++ PresentacionAspNetMvc/Controllers/FacturaController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TiendaVirtual.Entidades;
+using TiendaVirtual.LogicaNegocio;
+
+
+namespace PresentacionAspNetMvc.Controllers
+{
+    public class FacturaController : Controller
+    {
+        // GET: Factura
+
+        public ActionResult MFactura()
+        {
+            ICarrito carrito = (ICarrito)HttpContext.Session["carrito"];
+            ILogicaNegocio ln = new LogicaNegocio();
+            IFactura f = ln.FacturarCarrito(carrito);
+            return View("Index", f);
+        }
+    }
+}
